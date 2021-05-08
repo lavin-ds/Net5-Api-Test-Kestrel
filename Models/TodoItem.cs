1dfbb746538199fad1806deeeb8c7bb6b10de4f8
lds <lifeslong@gmail.com>
2021-05-08 16:12:08 +0200

    Base code for api

---
+++ Models/TodoItem.cs
@@ -0,0 +1,10 @@
+namespace ProjServer.Models
+{
+    public class TodoItem
+    {
+        public long Id { get; set;}
+        public string name { get; set;}
+
+        public bool IsComplete {get; set;}
+    }
+}
